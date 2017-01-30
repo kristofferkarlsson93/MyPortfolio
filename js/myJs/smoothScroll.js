69d61b5d2cb5f64fd35ac718114d3fdfd5fdb32b
Kristoffer Karlsson <kristoffer.karlsson93@hotmail.com>
2017-01-30 22:25:57 +0100

    Fixed the bugg with the scroll

--- js/myJs/smoothScroll.js
+++ js/myJs/smoothScroll.js
@@ -4,13 +4,19 @@
 
 $(document).ready(function() {
     $(".menuItem").click(function(event) {
-        var link = $(this).attr("href");
-        console.log($(link).offset().left);
-        $("html, body, #mainContent").stop().animate({
-          scrollLeft: $(link).offset().left
-        });
+        var $link = $(this).attr("href");  //Takes the link
+        console.log($($link).offset().left)
+        console.log("andra: " + $("#mainContent").scrollLeft());
+        $("#mainContent").animate({ //animates the scrolling
+            //Find current scroll position and add that.
+
+            scrollLeft: $("#mainContent").scrollLeft() + $($link).offset().left
+        }, 500);
         event.preventDefault();
     });
 });
 
+// kolla denna
+//http://www.queness.com/post/356/create-a-vertical-horizontal-and-diagonal-sliding-content-website-with-jquery
+//https://tympanus.net/codrops/2010/06/02/smooth-vertical-or-horizontal-page-scrolling-with-jquery/
 //Fix bug with the numbers
