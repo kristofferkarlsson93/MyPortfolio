65b6d45f210a50f8b6023e6f6f57dbf049abc0f7
Kristoffer Karlsson <kristoffer.karlsson93@hotmail.com>
2017-02-07 21:03:14 +0100

    Fixed a bugg with mobile-friendlyness

--- js/myJs/header.js
+++ js/myJs/header.js
@@ -8,13 +8,13 @@ function changeHeaderSize() {
     var currentPosition = window.pageYOffset || document.documentElement.scrollTop; //offset is the one doing shit
     var header = document.getElementById("jumboDiv");
     if (previousPosition < currentPosition && !scrollFlag) {
-        console.log('scrolling down');
+        //console.log('scrolling down');
         header.className += " changeOnDownScroll";
         scrollFlag = true;
     } else if(previousPosition > currentPosition && scrollFlag){
         scrollFlag = false
         header.className = "jumbotron";
-        console.log('scrolling up');
+        //console.log('scrolling up');
     }
 
     previousPosition = currentPosition;
--- js/myJs/smoothScroll.js
+++ js/myJs/smoothScroll.js
@@ -4,10 +4,24 @@
 
 $(document).ready(function() {
     $(".menuItem").click(function(event) {
-        var $link = $(this).attr("href");  //Takes the link
-        $("#mainContent").animate({ //animates the scrolling
-            scrollLeft: $("#mainContent").scrollLeft() + $($link).offset().left
-        }, 500);
-        event.preventDefault();
+
+        //On bigscreen-devices.
+        if ($(window).width() > 768) {
+            console.log($(window).width())
+            var $link = $(this).attr("href");
+            $("#mainContent").animate({
+                scrollLeft: $("#mainContent").scrollLeft() + $($link).offset().left
+            }, 500);
+            event.preventDefault();
+        }
+
+        //On smaller screens.
+        else {
+            var $link = $(this).attr("href");  //Takes the link
+            $("#mainContent").animate({ //animates the scrolling
+                scrollTop: $("#mainContent").scrollTop() + $($link).offset().top
+            }, 500);
+            event.preventDefault();
+        }
     });
 });
