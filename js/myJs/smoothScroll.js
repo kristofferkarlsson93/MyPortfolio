176fc9ebdc841e55fb45a6bbe8cb3962d69deccb
Kristoffer Karlsson <kristoffer.karlsson93@hotmail.com>
2017-02-22 13:35:22 +0100

    some minor fixes with mobile friendlynes

--- js/myJs/smoothScroll.js
+++ js/myJs/smoothScroll.js
@@ -6,15 +6,15 @@ $(document).ready(function() {
     $(".menuItem").click(function(event) {
 
         //On bigscreen-devices.
-        if ($(window).width() > 768) {
-            console.log($(window).width())
-            var $link = $(this).attr("href");
-            $("#mainContent").animate({
-                scrollLeft: $("#mainContent").scrollLeft() + $($link).offset().left
-            }, 500);
-            event.preventDefault();
-        }
 
+        console.log($(window).width())
+        var $link = $(this).attr("href");
+        $("#mainContent").animate({
+            scrollLeft: $("#mainContent").scrollLeft() + $($link).offset().left
+        }, 500);
+        event.preventDefault();
+
+        /*
         //On smaller screens.
         else {
             var $link = $(this).attr("href");  //Takes the link
@@ -22,6 +22,6 @@ $(document).ready(function() {
                 scrollTop: $("#mainContent").scrollTop() + $($link).offset().top
             }, 500);
             event.preventDefault();
-        }
+        }*/
     });
 });
